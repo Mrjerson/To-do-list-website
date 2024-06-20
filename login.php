6463d907cd3a9846d79a9db9fb79f3d94ef0af12
Mrjerson <doronilajerson@gmail.com>
2024-06-20 18:48:18 +0800

    Updated

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+if(isset($_SESSION['username'])){
+    header('Location: welcome.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <form action="login1.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
